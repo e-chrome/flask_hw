bd67c7a376e8e9b826b02f5150dd31232f473c1f
Евгений Хромов <ex.codding@gmail.com>
2022-10-06 22:23:08 +0300

    удален комментарий

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ celery = Celery(
 celery.conf.update(app.config)
 
 
-# class ContextTask(celery.Task):
-#     def __call__(self, *args, **kwargs):
-#         with app.app_context():
-#             return self.run(*args, **kwargs)
-#
-# celery.Task = ContextTask
+class ContextTask(celery.Task):
+    def __call__(self, *args, **kwargs):
+        with app.app_context():
+            return self.run(*args, **kwargs)
+
+celery.Task = ContextTask
 
 
